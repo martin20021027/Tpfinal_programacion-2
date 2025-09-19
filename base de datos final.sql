c8e80c5b1cd1571501509524ea323fee78a0be4a
martin20021027 <tincho27argalas@gmail.com>
2025-09-19 19:49:26 -0300

    Add files via upload

---
+++ base de datos final.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `cliente` (
+  `ID_cliente` int NOT NULL AUTO_INCREMENT,
+  `nombre_apellido` varchar(50) NOT NULL,
+  `numero_reserva` int NOT NULL,
+  PRIMARY KEY (`ID_cliente`)
+) 
+
+CREATE TABLE `reserva` (
+  `ID_reserva` int NOT NULL AUTO_INCREMENT,
+  `estado_mesa` varchar(50) NOT NULL,
+  `capacidad_mesa` int NOT NULL,
+  `numero_mesa` int NOT NULL,
+  `numero_reserva` int NOT NULL,
+  PRIMARY KEY (`ID_reserva`) USING BTREE
+) 
+
+CREATE TABLE  `usuarios` (
+  `ID_usuario` int NOT NULL AUTO_INCREMENT,
+  `nombre_apellido` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID_usuario`)
+) 
